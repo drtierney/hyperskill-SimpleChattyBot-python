57d491520e1552c9005f014de36d5dd1dfd11912
drtierney <d.r.tierney@hotmail.co.uk>
2021-03-14 18:57:38 +0000

    Stage #2 completed
    Stage 2/5: Print your name

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,6 +1,10 @@
-# write your code here
 bot_name = "Monty"
 birth_year = 2021
 
 print("Hello! My name is {0}.".format(bot_name))
 print("I was created in {0}.".format(birth_year))
+print("Please, remind me your name.")
+
+# reading a name
+your_name = input()
+print("What a great name you have, {0}!".format(your_name))
